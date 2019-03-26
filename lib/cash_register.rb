d3fd90ac4cae68d4429065c0f884cdd36759137a
Dr. XyClo <sdcarmody@gmail.com>
2019-03-26 23:47:41 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -34,7 +34,7 @@ class CashRegister
   
 ## need class method because an instance method is operating on the instance of the add_item which is tomoat, it's why egg never shows up and why adding potato "overwrites" tomato.  The test is inializing a new CashRegister object and adding an sitnace of eggs and an intsance of 3 tomatoes.  I want to create.  
 
-  def self.items 
+  def items 
    # list of object titles, or list of titles for one object 
    
    @@items_array << CashRegister.title  
